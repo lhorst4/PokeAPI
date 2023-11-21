faa97bc2394a74d81560aaa8497cec5074f92bbc
Levi Horst <levi.horst@bruins.belmont.edu>
2023-11-21 14:33:44 -0600

    got api to work and got images to work

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             char[] invalid_chars = "%&*(@)!;:<>".toCharArray();
-            String pokemon = searchET.getText().toString();
+            String pokemon = searchET.getText().toString().toLowerCase();
             for(int i = 0; i < invalid_chars.length; i++){
                 String check = String.valueOf(invalid_chars[i]);
                 if(pokemon.contains(check)){
@@ -45,30 +45,57 @@ public class MainActivity extends AppCompatActivity {
 
             try{
                 int dexNum = Integer.parseInt(searchET.getText().toString());
-                if(dexNum >= 1010 || dexNum < 1){
-                    Toast.makeText(getApplicationContext(), "Search using a number between 1 and 1010", Toast.LENGTH_LONG).show();
+                if(dexNum > 1017 || dexNum < 1){
+                    Toast.makeText(getApplicationContext(), "Search using a number between 1 and 1017", Toast.LENGTH_LONG).show();
                     return;
                 }
                 Log.i("SearchType", "Number");
             }catch(Exception e){
                 Log.i("SearchType", "Name");
             }
-            String imageURL = "https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/001.png";
-            Picasso.get().load(imageURL).into(pokeIV);
             makeRequest(pokemon);
         }
     };
 
     private void makeRequest(String pokemon) {
-        ANRequest req = AndroidNetworking.get("https://pokeapi.co/api/v2/pokemon/{pokemon}")
-                .addPathParameter(searchET.getText().toString().toLowerCase(), pokemon)
-                .setPriority(Priority.LOW)
-                .build();
+        ANRequest req = AndroidNetworking.get("https://pokeapi.co/api/v2/pokemon/"+pokemon)
+                .setPriority(Priority.LOW).build();
+
         // Log.i("URL", "https://pokeapi.co/api/v2/pokemon/"+pokemon);
         req.getAsJSONObject(new JSONObjectRequestListener() {
             @Override
             public void onResponse(JSONObject pokemons) {
                 Log.i("Data Request", "Data received.");
+                try {
+                    String pokename = pokemons.getString("name");
+                    int pokedexnum = pokemons.getInt("id");
+                    int pokeheight = pokemons.getInt("height");
+                    int pokeweight = pokemons.getInt("weight");
+                    int basexp;
+                    try {
+                        basexp = pokemons.getInt("base_experience");
+                    }catch(Exception e){
+                        basexp = 0;
+                    }
+                    JSONArray pokeabilities = pokemons.getJSONArray("abilities");
+                    JSONObject abilities = pokeabilities.getJSONObject(0);
+                    JSONObject abilityobj = abilities.getJSONObject("ability");
+                    String ability = abilityobj.getString("name");
+                    JSONArray movearr = pokemons.getJSONArray("moves");
+                    JSONObject moveobj = movearr.getJSONObject(0);
+                    JSONObject moveobj_2 = moveobj.getJSONObject("move");
+                    String move = moveobj_2.getString("name");
+                    nameTV.setText(pokename);
+                    numberTV.setText(String.valueOf(pokedexnum));
+                    weightTV.setText(String.valueOf(pokeweight));
+                    heightTV.setText(String.valueOf(pokeheight));
+                    xpTV.setText(String.valueOf(basexp));
+                    abilityTV.setText(ability);
+                    moveTV.setText(move);
+                    setImage(pokedexnum);
+                }catch(Exception e){
+                    Log.i("Exception", "JSON error");
+                }
             }
 
             @Override
@@ -112,5 +139,23 @@ public class MainActivity extends AppCompatActivity {
 
         searchBT.setOnClickListener(searchListener);
 
+    }
+    public void setImage(int pokedexnum){
+        String pokedexnumstr = "";
+        if(pokedexnum < 10){
+            pokedexnumstr = "00" + pokedexnum;
+        }else if(pokedexnum < 100){
+            pokedexnumstr = "0" + pokedexnum;
+        }else{
+            pokedexnumstr = String.valueOf(pokedexnum);
+        }
+
+        if(pokedexnum < 906) {
+            String imageURL = "https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/" + pokedexnumstr + ".png";
+            Picasso.get().load(imageURL).into(pokeIV);
+        }else{
+            String imageURL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+pokedexnumstr+".png";
+            Picasso.get().load(imageURL).into(pokeIV);
+        }
     }
 }
